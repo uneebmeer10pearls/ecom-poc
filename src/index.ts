58fc10d096f0b311855687749ca3ba57aaca4aed
Uneeb Saleem <uneeb.saleem@tenpearls.com>
2023-05-29 15:10:40 +0500

    schema completion

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-// import { PrismaClient } from '@prisma/client'
-// const prisma = new PrismaClient() 
+import { PrismaClient } from '@prisma/client'
+import typeDefs from './graphql/schema.js';
+const prisma = new PrismaClient() 
 
 // import express from 'express';
 
@@ -26,38 +27,39 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+// const typeDefs = `#graphql
+//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
+//   # This "Book" type defines the queryable fields for every book in our data source.
+//   type Book {
+//     name: String
+//   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
+//   # The "Query" type is special: it lists all of the available queries that
+//   # clients can execute, along with the return type for each. In this
+//   # case, the "books" query returns an array of zero or more Books (defined above).
+//   type Query {
+//     books: [Book]
+//   }
+// `;
 
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
+// const books = [
+//   {
+//     title: 'The Awakening',
+//     author: 'Kate Chopin',
+//   },
+//   {
+//     title: 'City of Glass',
+//     author: 'Paul Auster',
+//   },
+// ];
 
 
 const resolvers = {
   Query: {
-    books: () => books,
+    books: async () => {
+      return await prisma.user.findMany()
+    },
   },
 };
 
