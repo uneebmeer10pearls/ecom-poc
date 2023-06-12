892e05abe38a5d05d0e339c23377fff1d6a56009
Uneeb <uneeb.saleem@10pearls.com>
2023-06-12 13:56:44 +0500

    github actions test #66

--- src/__tests__/product.test.ts
+++ src/__tests__/product.test.ts
@@ -5,7 +5,7 @@ describe("Login User", () => {
     describe("given the user exist", () => {
       it("should return a 200", async () => {
         const user={
-          "email":"testemail@gmail.com",
+          "email":"uneebmir23@gmail.com",
           "password":"password"
       }
         const res = await supertest(app).post(`/login`).send(user);
