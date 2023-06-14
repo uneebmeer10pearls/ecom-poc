0f2db2ef84681dbd653963879c61c68664c5f0f4
Uneeb <uneeb.saleem@10pearls.com>
2023-06-14 14:53:07 +0500

    github actions test #74

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -11,7 +11,7 @@ import { Router } from 'express'
 const app = Router();
 
 app.get('/', (req: Request, res: Response) => {
-    res.send("Hello world New");
+    res.send("Hello world Amir");
 });
  
 app.post("/signup",[
